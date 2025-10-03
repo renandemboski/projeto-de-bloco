2b8f08edf2b0a7f43b521eeceeb912b86697ea3c
arthurq310 <84025820+arthurq310@users.noreply.github.com>
2025-10-03 20:12:12 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import Button from "./components/ui/Button/Button";
+import Footer from "./components/Footer"; 
 
 function App() {
   return (
@@ -7,6 +8,8 @@ function App() {
       <img src="/logo.svg" alt="Logo Mind Care" />
       <h1>Mind Care</h1>
       <Button>Teste</Button>
+
+      <Footer />
     </>
   );
 }
