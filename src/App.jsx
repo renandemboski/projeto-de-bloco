f35ccf2c5f712dbdb2f3d6898934346991fe8a07
Renan Demboski <renandemboski@gmail.com>
2025-10-03 10:54:05 -0300

    Componente Button

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import Button from "./components/ui/Button/Button";
 
 function App() {
   return (
     <>
       <img src="/logo.svg" alt="Logo Mind Care" />
       <h1>Mind Care</h1>
+      <Button>Teste</Button>
     </>
   );
 }
