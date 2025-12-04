9972eeb1bb5eb02bd79d8f15d0ee2bba735ebd2d
arthurq310 <84025820+arthurq310@users.noreply.github.com>
2025-12-04 11:30:06 -0300

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,10 @@ import Home from "./pages/Home/Home";
 import Professional from "./pages/Professional/Professional";
 import SignIn from "./pages/Auth/Sign-in/Sign-in";
 import SignUp from "./pages/Auth/Sign-up/Sign-up";
+import Profile from "./pages/Profile/Profile";
+import Schedule from "./pages/Schedule/Schedule";
+import Appointments from "./pages/Appointments/Appointments";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   return (
@@ -13,6 +17,30 @@ function App() {
         <Route path="/professional" element={<Professional />}></Route>
         <Route path="/login" element={<SignIn />}></Route>
         <Route path="/register" element={<SignUp />}></Route>
+        <Route
+          path="/profile"
+          element={
+            <ProtectedRoute>
+              <Profile />
+            </ProtectedRoute>
+          }
+        ></Route>
+        <Route
+          path="/schedule"
+          element={
+            <ProtectedRoute>
+              <Schedule />
+            </ProtectedRoute>
+          }
+        ></Route>
+        <Route
+          path="/appointments"
+          element={
+            <ProtectedRoute>
+              <Appointments />
+            </ProtectedRoute>
+          }
+        ></Route>
       </Routes>
     </BrowserRouter>
   );
